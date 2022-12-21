a236238da0e8784ea3694303480be15ada653e82
aleks2908 <108425973+aleks2908@users.noreply.github.com>
2022-12-21 10:04:37 +0000

    Deploying to gh-pages from @ aleks2908/goit-js-hw-09@e109ea4e661a1223110afebae7782fd0c80e4631 🚀

---
+++ 01-color-switcher.80ef9f12.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-Stop]");t.addEventListener("click",(function(){t.disabled=!0,e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){t.disabled=!1,clearInterval(e)}));var e=null}();
+//# sourceMappingURL=01-color-switcher.80ef9f12.js.map
