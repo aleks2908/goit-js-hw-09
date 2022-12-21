e109ea4e661a1223110afebae7782fd0c80e4631
Oleksandr Druzhynin <aleks_str78@ukr.net>
2022-12-21 12:04:14 +0200

    перша готова

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stoptBtn = document.querySelector('button[data-Stop]');
 
+startBtn.addEventListener('click', startChangeBgColor);
+stoptBtn.addEventListener('click', stopChangeBgColor);
+
+let changeBgColorInterval = null;
+
+function startChangeBgColor() {
+  startBtn.disabled = true;
+  changeBgColorInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChangeBgColor() {
+  startBtn.disabled = false;
+  clearInterval(changeBgColorInterval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
